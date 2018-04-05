dbe6d477f87790f9b704ece140b6c14509c0ec30
andy tham <thamandyy@gmail.com>
2018-04-05 18:49:12 -0400

    carousel init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
+import CarouselPage from './CarouselPage.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <CarouselPage />
       </div>
     );
   }
---
+++ src/Carousel.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CarouselContainer from './CarouselContainer'
+import Wrapper from './Wrapper'
+import CarouselSlot from './CarouselSlot'
+import PropTypes from 'prop-types';
+
+class Carousel extends Component {
+  render() {
+    const { title, children } = this.props
+
+    return (
+      <div>
+        <h2>{ title }</h2>
+
+        <Wrapper>
+          <CarouselContainer>
+            { children.map((child, index) => (
+              <CarouselSlot
+                key={ index }
+              >
+                {child}
+              </CarouselSlot>
+            )) }
+          </CarouselContainer>
+        </Wrapper>
+      </div>
+    )
+  }
+}
+
+Carousel.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node
+};
+
+export default Carousel;
